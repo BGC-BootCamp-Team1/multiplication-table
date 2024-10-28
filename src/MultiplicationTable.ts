7956fed8ded246f0ec9249253e83e1cc3ffb8a6c
Ting Li <TLi26@slb.com>
2024-10-28 17:27:21 +0800

    feat: add overall structure

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,4 +1,31 @@
 export class MultiplicationTable {
+  public generateMultiplicationTable(start: number, end: number): string{
+    if(!this.secondInputLargerThanFirst(start, end)){
+      return "";
+    }else if(!this.inRange1to10(start, end)){
+      return "";
+    }else{
+      return this.render(start, end);
+    }
+  }
+
+  public secondInputLargerThanFirst(start: number, end: number): boolean{
+    return false;
+  }
+  
+  public inRange1to10(start: number, end: number): boolean
+  {
+    return false;
+  }
+  
+  public generateTable(start: number, end: number): string
+  {
+    var result = "";
+    
+    return result;
+  }
+  
+
   public render(start: number, end: number): string{
     return '1*1=1'
   }
