9a4f4655c5fa08d7c24fb1ea4fa13af734644dc3
htian15 <htian15@slb.com>
2024-10-28 19:24:09 +0800

    fix: align every column

--- src/MultiplicationTable.js
+++ src/MultiplicationTable.js
@@ -4,26 +4,44 @@ exports.MultiplicationTable = void 0;
 var MultiplicationTable = /** @class */ (function () {
     function MultiplicationTable() {
     }
-    MultiplicationTable.prototype.render = function (table) {
+    MultiplicationTable.prototype.renderTable = function (mulList) {
         var output = "";
-        // 找到每列的最大宽度
-        var colWidths = table[0].map(function (_, colIndex) {
-            return Math.max.apply(Math, table.map(function (row) { return row[colIndex].length; }));
-        });
-        for (var _i = 0, table_1 = table; _i < table_1.length; _i++) {
-            var row = table_1[_i];
-            var formattedRow = row.map(function (item, index) {
-                // 根据每列的最大宽度填充空格
-                return item.padEnd(colWidths[index] + 2); // +2 为了增加一点格式美观
-            }).join(''); // 使用空字符串连接
-            output += formattedRow + '\n'; // 每行添加换行符
+        var colLen = this.recordColumnLength(mulList[mulList.length - 1]);
+        console.log(colLen);
+        console.log(mulList.length);
+        console.log(mulList[mulList.length - 1].length);
+        for (var row = 0; row < mulList.length; row++) {
+            for (var col = 0; col < mulList[row].length; col++) {
+                output += mulList[row][col];
+                if (col != mulList[row].length - 1) {
+                    var space = " ";
+                    output += space.repeat(colLen[col] - mulList[row][col].length);
+                }
+            }
+            if (row != mulList.length - 1) {
+                output += "\n";
+            }
+        }
+        return output;
+    };
+    MultiplicationTable.prototype.render = function (start, end) {
+        if (!this.isValidInput(start, end)) {
+            return '';
+        }
+        // let output: string = '';
+        var table = this.generateMultiplicationTable(start, end);
+        var output = this.renderTable(table);
+        return output;
+    };
+    MultiplicationTable.prototype.recordColumnLength = function (row) {
+        var output = new Array();
+        for (var _i = 0, row_1 = row; _i < row_1.length; _i++) {
+            var singleMul = row_1[_i];
+            output.push(singleMul.length + 2);
         }
         return output;
     };
     MultiplicationTable.prototype.generateMultiplicationTable = function (start, end) {
-        if (!this.isValidInput(start, end)) {
-            return [];
-        }
         var table = [];
         for (var i = start; i <= end; i++) {
             var rowItems = this.generateMultiplicationRow(start, i);
@@ -40,7 +58,8 @@ var MultiplicationTable = /** @class */ (function () {
     };
     MultiplicationTable.prototype.generateMultiplicationItem = function (multiplierRow, multiplierColumn) {
         var product = multiplierRow * multiplierColumn;
-        return "".concat(multiplierRow, " * ").concat(multiplierColumn, " = ").concat(product);
+        // return `${multiplierRow}*${multiplierColumn}=${product}`;
+        return "".concat(multiplierColumn, "*").concat(multiplierRow, "=").concat(product);
     };
     MultiplicationTable.prototype.isValidInput = function (start, end) {
         return (this.isValidRange(start) &&
@@ -56,6 +75,12 @@ var MultiplicationTable = /** @class */ (function () {
     return MultiplicationTable;
 }());
 exports.MultiplicationTable = MultiplicationTable;
+// const table = new MultiplicationTable();
+// const generatedTable = table.generateMultiplicationTable(2, 1);
+// console.log(table.render(generatedTable));
 var table = new MultiplicationTable();
-var generatedTable = table.generateMultiplicationTable(2, 3);
-console.log(table.render(generatedTable));
+var start = 2;
+var end = 10;
+//when
+var rendered = table.render(start, end);
+console.log(rendered);
--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,31 +1,57 @@
 export class MultiplicationTable {
   
 
-  public render(table: string[][]): string {
+  private renderTable(mulList:string[][]): string {
     let output: string = "";
-
-    // 找到每列的最大宽度
-    const colWidths = table[0].map((_, colIndex) => {
-      return Math.max(...table.map(row => row[colIndex].length));
-    });
-
-    for (const row of table) {
-      const formattedRow = row.map((item, index) => {
-        // 根据每列的最大宽度填充空格
-        return item.padEnd(colWidths[index] + 2); // +2 为了增加一点格式美观
-      }).join(''); // 使用空字符串连接
-      output += formattedRow + '\n'; // 每行添加换行符
+    let colLen: Array<number> = this.recordColumnLength(mulList[mulList.length - 1]);
+    console.log(colLen);
+    console.log(mulList.length);
+    console.log(mulList[mulList.length-1].length);
+    for (let row = 0 ; row < mulList.length ; row++)
+    {
+      
+      for (let col = 0 ; col < mulList[row].length ; col++) {
+        output += mulList[row][col];
+        if (col != mulList[row].length - 1) {
+          let space = " ";
+          output += space.repeat(colLen[col] - mulList[row][col].length);
+        }
+      }
+      if (row != mulList.length - 1) {
+        output += "\n";
+      }
     }
+    return output;
+  }
+
+
+  public render(start: number, end: number): string {
+    if (!this.isValidInput(start, end)) {
+      return '';
+
+    }
+    
+    // let output: string = '';
+    let table = this.generateMultiplicationTable(start, end);
+
+    let output = this.renderTable(table);
 
     return output;
   
   }
 
-  public generateMultiplicationTable(start: number, end: number): string[][] {
-    if (!this.isValidInput(start, end)) {
-      return [];
-
+  private recordColumnLength(row: Array<string>): Array<number> {
+    let output = new Array<number>();
+    for (let singleMul of row){
+      output.push(singleMul.length + 2);
     }
+    return output;
+}
+
+
+
+  public generateMultiplicationTable(start: number, end: number): string[][] {
+    
     const table: string[][] = [];
     for (let i = start; i <= end; i++) {
       const rowItems = this.generateMultiplicationRow(start, i);
@@ -50,7 +76,8 @@ export class MultiplicationTable {
     multiplierColumn: number
   ): string {
     const product = multiplierRow * multiplierColumn;
-    return `${multiplierRow} * ${multiplierColumn} = ${product}`;
+    // return `${multiplierRow}*${multiplierColumn}=${product}`;
+    return `${multiplierColumn}*${multiplierRow}=${product}`;
   }
 
   public isValidInput(start: number, end: number): boolean {
@@ -71,6 +98,14 @@ export class MultiplicationTable {
 }
 
 
-const table = new MultiplicationTable();
-const generatedTable = table.generateMultiplicationTable(2, 3);
-console.log(table.render(generatedTable));
+// const table = new MultiplicationTable();
+// const generatedTable = table.generateMultiplicationTable(2, 1);
+// console.log(table.render(generatedTable));
+
+const table = new MultiplicationTable()
+    const start = 2
+    const end = 10
+
+    //when
+    const rendered = table.render(start, end);
+    console.log(rendered);
