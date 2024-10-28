3804b30ba531429fec6918f628ad653afc7bcea1
Sheng Liu <SLiu40@slb.com>
2024-10-28 18:50:32 +0800

    FIX.contactMultiplicationFrom func not corrt

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,6 +1,12 @@
 export class MultiplicationTable {
   public render(start: number, end: number): string{
-    return '1*1=1'
+    if (
+      !(this.isValid(start,end) && this.isInRange(start) && this.isInRange(end))
+    ) {
+      return '';
+    }
+    let martix :number[][]=this.generateTable(start,end);
+    return this.contactMultiplicationFrom(start,end,martix);
   }
 
   private isValid(start: number, end: number):boolean{
@@ -21,17 +27,24 @@ export class MultiplicationTable {
     return martix;
   }
 
-  private contactMultiplicationFrom(start: number, end: number,martix:number[][]):string{
+  private contactMultiplicationFrom(start: number, end: number,matrix:number[][]):string{
     let res:string = '';
-    for (let index = start; index < end; index++) {
-      res=res+`${martix[index][0]}*${martix[index][1]}=${martix[index][0]*martix[index][1]}`
-      for (let j = 0; j < index; j++) {
-        if (j=index-1) {
-          res=res+'\n'
-        }else{
-          res=res+'\t'
+    let cursor:number=0;
+    for (let index = start; index <= end; index++) {
+      for (let j = start; j <= index; j++) {
+        if (cursor < matrix.length) {
+          res += `${matrix[cursor][0]}*${matrix[cursor][1]}=${matrix[cursor][0] * matrix[cursor][1]}`;
+
+          if (j === index) {
+              res += '\n';
+          } else {
+              res += '\t';
+          }
+          cursor++;
+  
         }
       }
+
     }
     return res;
   }
