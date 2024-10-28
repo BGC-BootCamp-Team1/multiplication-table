5e7bc0f36bf85a3f11661c460600d7fd04e542a2
Ting Li <TLi26@slb.com>
2024-10-28 17:31:35 +0800

    feat: implement inRange1to10 and reformat

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,32 +1,31 @@
 export class MultiplicationTable {
-  public generateMultiplicationTable(start: number, end: number): string{
-    if(!this.secondInputLargerThanFirst(start, end)){
+  public generateMultiplicationTable(start: number, end: number): string {
+    if (!this.secondInputLargerThanFirst(start, end)) {
       return "";
-    }else if(!this.inRange1to10(start, end)){
+    } else if (!this.inRange1to10(start)) {
       return "";
-    }else{
+    } else if (!this.inRange1to10(end)) {
+      return "";
+    } else {
       return this.render(start, end);
     }
   }
 
-  public secondInputLargerThanFirst(start: number, end: number): boolean{
-    return end>=start;
+  public secondInputLargerThanFirst(start: number, end: number): boolean {
+    return end >= start;
   }
-  
-  public inRange1to10(start: number, end: number): boolean
-  {
-    return false;
+
+  public inRange1to10(num: number): boolean {
+    return num >= 1 || num <= 10;
   }
-  
-  public generateTable(start: number, end: number): string
-  {
+
+  public generateTable(start: number, end: number): string {
     var result = "";
-    
+
     return result;
   }
-  
 
-  public render(start: number, end: number): string{
-    return '1*1=1'
+  public render(start: number, end: number): string {
+    return "1*1=1";
   }
 }
