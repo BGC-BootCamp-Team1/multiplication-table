820f3d247a65ca3c20c5f44e53027021374abad9
Sheng Liu <SLiu40@slb.com>
2024-10-28 16:50:29 +0800

    FEAT.add isValid function

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -2,4 +2,9 @@ export class MultiplicationTable {
   public render(start: number, end: number): string{
     return '1*1=1'
   }
+
+  private isValid(start: number, end: number):boolean{
+    return start<=end;
+  }
+
 }
