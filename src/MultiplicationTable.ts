95bd61efd3b801dc3db80af54b722c717e995283
htian15 <htian15@slb.com>
2024-10-28 17:34:12 +0800

    feat:isValidInput

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -1,5 +1,25 @@
 export class MultiplicationTable {
-  public render(start: number, end: number): string{
-    return '1*1=1'
+  public render(start: number, end: number): string {
+    return "1*1=1";
   }
+
+  
+
+  public isValidInput(start: number, end: number): boolean {
+    return (
+      this.isValidRange(start) &&
+      this.isValidRange(end) &&
+      this.isStartSmallerThanEnd(start, end)
+    );
+  }
+
+  public isStartSmallerThanEnd(start: number, end: number): boolean {
+    return start < end;
+  }
+
+  public isValidRange(num: number): boolean {
+    return num >= 1 && num <= 10;
+  }
+
+  
 }
