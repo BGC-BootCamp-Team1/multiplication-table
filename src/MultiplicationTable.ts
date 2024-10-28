a0e63a7179133c819eceb3f4732b26f0e5d02235
tangqi <qtang5@slb.com>
2024-10-28 18:54:16 +0800

    fix: correct boundary error in genMultiplicationRow and genMultiplicationTable

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -13,15 +13,15 @@ export class MultiplicationTable {
   }
   public genMultiplicationRow(start:number,rowNumber:number):string[]{
     let row:string[] = [];
-    for(let i = start;i<rowNumber;i++){
-      row.push(start+'*'+i+'='+start*i);
+    for(let i = start;i<=rowNumber;i++){
+      row.push(i+'*'+rowNumber+'='+i*rowNumber);
     }
     return row;
   }
   public genMultiplicationTable(start:number,end:number):string[][]{
-    let multiplicationTable:string[][]=[];
-    for(let i=start;i<end;i++){
-      multiplicationTable[i]=this.genMultiplicationRow(start,i);
+    let multiplicationTable:string[][] = [];
+    for(let i=start;i<=end;i++){
+      multiplicationTable[i-start]=this.genMultiplicationRow(start,i);
     }
     return multiplicationTable
   }
