5b6a5911ebb7e30213340c3cd6ea9f7b9faac937
tangqi <qtang5@slb.com>
2024-10-28 17:28:24 +0800

    feat: add isInputValid method

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -2,4 +2,12 @@ export class MultiplicationTable {
   public render(start: number, end: number): string{
     return '1*1=1'
   }
+  public isInputValid(start:number,end:number):boolean{
+    if(start<=end && start>=0 && end<=10) 
+      return true
+    else 
+      return false
+  }
+  
+
 }
