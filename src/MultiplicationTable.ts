c1c8bc1441c42aeaca758ddf83f24f08c0ededf6
Anyan Zhao <AZhao4@slb.com>
2024-10-28 17:21:37 +0800

    feat: implement isInRange

--- src/MultiplicationTable.ts
+++ src/MultiplicationTable.ts
@@ -32,7 +32,7 @@ export class MultiplicationTable {
     return true;
   }
   private isInRange(num: number): boolean {
-    throw new Error("Method not implemented.");
+    return num >= 1 && num <= 10;
   }
   private isStartSmallerThanEnd(start: number, end: number): boolean {
     throw new Error("Method not implemented.");
